96793dafb921657d40acfcf799d6fae26aa56ea4
nizihad824 <mdnurulislam97@gmail.com>
2022-04-21 01:56:17 +0200

    created route for components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle}=useFirebase()
     return (
         <div>
-            <h2>Please Log in</h2>
+            <h3>Please Login</h3>
+            <div style={{margin: '20px'}}>
+            <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
             
+            <form>
+                <input type="email" placeholder='Your Email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Password'/>
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
