b9e4d2cbba2e1a3e7d364c9182e50e1a6232694c
nizihad824 <mdnurulislam97@gmail.com>
2022-04-21 02:25:38 +0200

    login with google

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import './Header.css'
 
 const Header = () => {
+    const { user } = useFirebase()
     return (
         <div className='header'>
 
             <nav>
                 <Link to="/home">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
+
                 <Link to="/products">Products</Link>
                 <Link to="/orders">Orders</Link>
                 <Link to="/review">Review</Link>
+                <Link to="/register">Register</Link>
+
+
+                {user?.uid
+                    ?
+                    <button>sign out</button>
+                    :
+                    <Link to="/login">Login</Link>
+                }
+
             </nav>
 
         </div>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,17 +1,28 @@
 import { useState } from "react"
+import app from '../firebase.init'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 
+const auth = getAuth(app)
+const googleProvider = new GoogleAuthProvider();
 
-const useFirebase =()=>{
-    const [user,setUser]=useState();
+const useFirebase = () => {
 
-    const signInWithGoogle =()=>{
-        console.log('signing in soon');
-    }
+    const [user, setUser] = useState();
+
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const user = result.user ;
+                setUser(user)
+                console.log(user);
+            })
+
+            }
     return {
-        user,
-        signInWithGoogle
-    }
+            user,
+            signInWithGoogle
+        }
 
-}
-export default useFirebase
+    }
+    export default useFirebase
